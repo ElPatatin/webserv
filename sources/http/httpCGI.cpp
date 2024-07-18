6dc6219717e3402be0cc7a1927775115eebd027c
Pol Ramos Martí <pramos-m@car13s6.42barcelona.com>
2024-07-18 19:42:20 +0200

    {Changes Before Disaster} CGI Env Vars

--- sources/http/httpCGI.cpp
+++ sources/http/httpCGI.cpp
@@ -6,58 +6,47 @@
 /*   By: pramos-m <pramos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 12:32:50 by cpeset-c          #+#    #+#             */
-/*   Updated: 2024/07/18 15:47:13 by pramos-m         ###   ########.fr       */
+/*   Updated: 2024/07/18 19:41:31 by pramos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "http.hpp"
 #include <sys/wait.h>
 #include <unistd.h>
-
+#include <map>
+char **createCGIEnvironment(const std::string &scriptPath, const std::string &queryString, Data &data, ConfigData &config, HttpData &http);
 void Http::executeCGI( const std::string & scriptPath, const std::string & queryString, std::ostringstream & response_stream, Data & data, ConfigData & config, HttpData & http )
 {
     LOG( INFO ) << ft::prettyPrint( __FUNCTION__, __LINE__, "Executing CGI script" );
 
-    LOG( INFO ) << ft::prettyPrint( __FUNCTION__, __LINE__, "Creating pipe" );
     int pipe_fd[ 2 ];
     if ( pipe( pipe_fd ) == -1 )
         return ( HttpErrors::sendError( data, INTERNAL_SERVER_ERROR, config ) );
 
-    LOG( INFO ) << ft::prettyPrint( __FUNCTION__, __LINE__, "Forking process" );
     int pid = fork();
     if ( pid == -1 )
         return ( HttpErrors::sendError( data, INTERNAL_SERVER_ERROR, config ) );
 
-    LOG( INFO ) << ft::prettyPrint( __FUNCTION__, __LINE__, "Child process" );
     if ( pid == 0 )
     {
         if ( close( pipe_fd[ 0 ] ) == -1 || dup2( pipe_fd[ 1 ], STDOUT_FILENO ) == -1 || close( pipe_fd[ 1 ] ) == -1 )
             std::exit(EXIT_FAILURE);
+        
+        char **envp = createCGIEnvironment(scriptPath, queryString, data, config, http);
+        char *argv[] = {(char *)scriptPath.c_str(), NULL};
+        
+        LOG(DEBUG) << ft::prettyPrint(__FUNCTION__, __LINE__, "Executing CGI script: " + scriptPath);
 
-        LOG( INFO ) << ft::prettyPrint( __FUNCTION__, __LINE__, "Setting environment variables" );
-        std::string query = "QUERY_STRING=" + queryString;
-        std::string contentLength = "CONTENT_LENGTH=" + http.headers[ "Content-Length" ].second;
-        std::string contentType = "CONTENT_TYPE=" + http.headers[ "Content-Type" ].second;
-
-        LOG( INFO ) << ft::prettyPrint( __FUNCTION__, __LINE__, "Executing script" );
-        char *argv[] = { (char *)scriptPath.c_str(), NULL };
-        char *envp[] = { (char *)query.c_str(), (char *)contentLength.c_str(), (char *)contentType.c_str(), NULL };
-
-        LOG( DEBUG ) << ft::prettyPrint( __FUNCTION__, __LINE__, "Executing CGI script: " + scriptPath );
-        LOG( DEBUG ) << ft::prettyPrint( __FUNCTION__, __LINE__, "Arguments: " + query + ", " + contentLength + ", " + contentType );
-
-        execve( scriptPath.c_str(), argv, envp );
-        LOG( ERROR ) << ft::prettyPrint( __FUNCTION__, __LINE__, "Failed to execute CGI script" );
-        std::exit( EXIT_FAILURE );
+        execve(scriptPath.c_str(), argv, envp);
+        LOG(ERROR) << ft::prettyPrint(__FUNCTION__, __LINE__, "Failed to execute CGI script");
+        std::exit(EXIT_FAILURE);
     }
     else
     {
-        LOG( INFO ) << ft::prettyPrint( __FUNCTION__, __LINE__, "Parent process" );
 
         if ( close( pipe_fd[ 1 ] ) == -1)
             return ( HttpErrors::sendError( data, INTERNAL_SERVER_ERROR, config ) );
 
-        LOG( INFO ) << ft::prettyPrint( __FUNCTION__, __LINE__, "Reading from pipe" );
         char buffer[ 4096 ];
         int bytesRead;
         while ( ( bytesRead = read( pipe_fd[ 0 ], buffer, sizeof( buffer ) ) ) > 0 )
@@ -70,12 +59,10 @@ void Http::executeCGI( const std::string & scriptPath, const std::string & query
         if ( close( pipe_fd[ 0 ]) == -1 )
             return ( HttpErrors::sendError( data, INTERNAL_SERVER_ERROR, config ) );
 
-        LOG( INFO ) << ft::prettyPrint( __FUNCTION__, __LINE__, "Waiting for child process" );
         int status;
         if ( waitpid( pid, &status, 0 ) == -1 )
             return ( HttpErrors::sendError( data, INTERNAL_SERVER_ERROR, config ) );
 
-        LOG( INFO ) << ft::prettyPrint( __FUNCTION__, __LINE__, "Checking child process status" );
         if ( WIFEXITED( status ) && WEXITSTATUS( status ) != 0 )
             return ( HttpErrors::sendError( data, INTERNAL_SERVER_ERROR, config ) );
 
@@ -88,4 +75,40 @@ void Http::executeCGI( const std::string & scriptPath, const std::string & query
             data.response = response_stream.str();
     }
     return ;
+}
+
+char **createCGIEnvironment(const std::string &scriptPath, const std::string &queryString, Data &data, ConfigData &config, HttpData &http) {
+    std::string query = "QUERY_STRING=" + queryString;
+    std::string contentLength = "CONTENT_LENGTH=" + http.headers["Content-Length"].second;
+    std::string contentType = "CONTENT_TYPE=" + http.headers["Content-Type"].second;
+    std::string serverSoftware = "SERVER_SOFTWARE=webserv/1.0";
+    std::string serverName = NULL;
+    std::string gatewayInterface = "GATEWAY_INTERFACE=CGI/1.1";
+    std::string serverProtocol = "SERVER_PROTOCOL=HTTP/1.1";
+    std::string serverPort = "SERVER_PORT=" + std::to_string(config.getPort());
+    std::string requestMethod = "REQUEST_METHOD=" + http.method;
+    std::string scriptName = "SCRIPT_NAME=" + scriptPath;
+    std::string pathInfo = NULL;
+    std::string pathTranslated = NULL;
+    std::string remoteAddr = NULL;
+    std::string remoteHost = NULL;
+
+    char **envp = new char *[16]; // Número de variables +1 para NULL
+    envp[0] = strdup(query.c_str());
+    envp[1] = strdup(contentLength.c_str());
+    envp[2] = strdup(contentType.c_str());
+    envp[3] = strdup(serverSoftware.c_str());
+    envp[4] = strdup(serverName.c_str());
+    envp[5] = strdup(gatewayInterface.c_str());
+    envp[6] = strdup(serverProtocol.c_str());
+    envp[7] = strdup(serverPort.c_str());
+    envp[8] = strdup(requestMethod.c_str());
+    envp[9] = strdup(scriptName.c_str());
+    envp[10] = strdup(pathInfo.c_str());
+    envp[11] = strdup(pathTranslated.c_str());
+    envp[12] = strdup(remoteAddr.c_str());
+    envp[13] = strdup(remoteHost.c_str());
+    envp[14] = NULL; // Terminador de la lista de variables
+
+    return envp;
 }
