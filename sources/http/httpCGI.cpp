939926da64c21ca3e85c9c5224c953cba8da9b90
Pol Ramos Martí <pramos-m@car15s4.42barcelona.com>
2024-07-21 16:14:35 +0200

    cgidone but py errors

--- sources/http/httpCGI.cpp
+++ sources/http/httpCGI.cpp
@@ -6,7 +6,7 @@
 /*   By: pramos-m <pramos-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 12:32:50 by cpeset-c          #+#    #+#             */
-/*   Updated: 2024/07/19 19:36:34 by pramos-m         ###   ########.fr       */
+/*   Updated: 2024/07/21 16:11:46 by pramos-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,50 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <map>
-char **createCGIEnvironment(const std::string &scriptPath, const std::string &queryString, Data &data, ConfigData &config, HttpData &http);
-void Http::executeCGI( const std::string & scriptPath, const std::string & queryString, std::ostringstream & response_stream, Data & data, ConfigData & config, HttpData & http )
+#include <fstream>
+
+void Http::executeCGI(const std::string &scriptPath, const std::string &queryString, std::ostringstream &response_stream, Data &data, ConfigData &config, HttpData &http)
 {
-    std::system("html/cgi-bin/list-files.py \"HTTP /1.1 GET \r\n\" > tmp.txt");
-    
+    (void)queryString; // Suppress unused parameter warning
+    (void)http; // Suppress unused parameter warning
+
     // 1.path 2. httpresponse(argument y redireccion a archivo, que todos los prints de py van a stdout y en consecuencia al file.)
     // En el py, con py y su libreria cgi hay que usar la libreria para parsear el http (lo hace solo)
     // Despues hay que devolver la response del gci. 
     
-    // en vez de ls poner el path del script
+    std::string tmpFilePath = "/tmp/cgi_output.txt"; // Archivo temporal para la salida del CGI
+    std::string command = scriptPath + " > " + tmpFilePath; // Comando para ejecutar el script y redirigir la salida al archivo temporal
+
+    // Ejecutar el comando
+    int result = std::system(command.c_str());
+    if (result == -1)
+    {
+        LOG(ERROR) << ft::prettyPrint(__FUNCTION__, __LINE__, "Failed to execute CGI script using std::system");
+        return HttpErrors::sendError(data, INTERNAL_SERVER_ERROR, config);
+    }
+
+    // Abrir el archivo temporal para leer la salida del script CGI
+    std::ifstream tmpFile(tmpFilePath.c_str()); // Cambiado a .c_str() para convertir std::string a const char*
+    if (!tmpFile.is_open())
+    {
+        LOG(ERROR) << ft::prettyPrint(__FUNCTION__, __LINE__, "Failed to open temporary file for CGI output");
+        return HttpErrors::sendError(data, INTERNAL_SERVER_ERROR, config);
+    }
+
+    // Leer el contenido del archivo temporal línea por línea y añadirlo al flujo de respuesta
+    std::string line;
+    while (std::getline(tmpFile, line))
+    {
+        response_stream << line << "\n";
+    }
+    tmpFile.close(); // Cerrar el archivo temporal
+
+    data.response = response_stream.str(); // Guardar la respuesta en la estructura de datos
+    LOG(DEBUG) << ft::prettyPrint(__FUNCTION__, __LINE__, "Response from CGI script: " + data.response);
+}
+
+        
+   // en vez de ls poner el path del script
    
     // LOG( INFO ) << ft::prettyPrint( __FUNCTION__, __LINE__, "Executing CGI script" );
 
@@ -83,57 +117,4 @@ void Http::executeCGI( const std::string & scriptPath, const std::string & query
     //         data.response = response_stream.str();
     // }
     // return ;
-}
 
-char **createCGIEnvironment(const std::string &scriptPath, const std::string &queryString, Data &data, ConfigData &config, HttpData &http) {
-    // std::string query = "QUERY_STRING=" + queryString;
-    // std::string contentLength = "CONTENT_LENGTH=" + http.headers["Content-Length"].second;
-    // std::string contentType = "CONTENT_TYPE=" + http.headers["Content-Type"].second;
-    // std::string serverSoftware = NULL;
-    // std::string serverName = NULL;
-    // std::string gatewayInterface = "GATEWAY_INTERFACE=CGI /1.1";
-    // std::string serverProtocol = "SERVER_PROTOCOL=HTTP /1.1";
-    // std::string serverPort = "SERVER_PORT=" + std::to_string(config.getPort());
-    // std::string requestMethod = "REQUEST_METHOD=" + http.method;
-    // std::string scriptName = "SCRIPT_NAME=" + scriptPath;
-    // std::string pathInfo = NULL;
-    // std::string pathTranslated = NULL;
-    // std::string remoteAddr = NULL;
-    // std::string remoteHost = NULL;
-
-    std::string query = "QUERY_STRING=" + queryString;
-    std::string contentLength = "CONTENT_LENGTH=" + http.headers["Content-Length"].second;
-    std::string contentType = "CONTENT_TYPE=" + http.headers["Content-Type"].second;
-    std::string serverSoftware = NULL;
-    std::string serverName = NULL;
-    std::string gatewayInterface = "GATEWAY_INTERFACE=CGI /1.1";
-    std::string serverProtocol = "SERVER_PROTOCOL=HTTP /1.1";
-    std::string serverPort = "SERVER_PORT=" + std::to_string(config.getPort());
-    std::string requestMethod = "REQUEST_METHOD=" + http.method;
-    std::string scriptName = "SCRIPT_NAME=" + scriptPath;
-    std::string pathInfo = NULL;
-    std::string pathTranslated = NULL;
-    std::string remoteAddr = NULL;
-    std::string remoteHost = NULL;
-
-    // std::string env = query + " " + contentLength + " " .....
-
-    // char **envp = new char *[16];
-    // envp[0] = strdup(query.c_str());
-    // envp[1] = strdup(contentLength.c_str());
-    // envp[2] = strdup(contentType.c_str());
-    // envp[3] = strdup(serverSoftware.c_str());
-    // envp[4] = strdup(serverName.c_str());
-    // envp[5] = strdup(gatewayInterface.c_str());
-    // envp[6] = strdup(serverProtocol.c_str());
-    // envp[7] = strdup(serverPort.c_str());
-    // envp[8] = strdup(requestMethod.c_str());
-    // envp[9] = strdup(scriptName.c_str());
-    // envp[10] = strdup(pathInfo.c_str());
-    // envp[11] = strdup(pathTranslated.c_str());
-    // envp[12] = strdup(remoteAddr.c_str());
-    // envp[13] = strdup(remoteHost.c_str());
-    // envp[14] = NULL;
-
-    return envp;
-}
